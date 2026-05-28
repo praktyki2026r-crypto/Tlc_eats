20c875ca4f39dc21c50d403e21b37a683309a12b
Praktyki <praktyki2026r@gmail.com>
2026-05-28 09:15:33 +0200

    fix: poprawiony seed bar wojtek

--- tlc_eats_app/management/commands/seed_menu_barwojtek.py
+++ tlc_eats_app/management/commands/seed_menu_barwojtek.py
@@ -7,28 +7,16 @@ MENU = {
         ('Drobiowy zestaw', 16.50, '2 kotlety, 3 gałki ziemniaków lub frytki, zestaw surówek', []),
         ('Ser prażony zestaw', 14.50, '2 kawałki sera, ziemniaki lub frytki', []),
         ('Wątróbka zestaw', 13.50, 'wątróbka z cebulką, ziemniaki lub frytki, zestaw surówek', []),
-        ('Placek po węgiersku', 16.50, 'zestaw surówek', []),
-        ('Placki ziemniaczane', 6.50, 'śmietana', []),
+        ('Placek po węgiersku', 16.50, 'Placek po węgiersku, zestaw surówek', []),
+        ('Placki ziemniaczane', 6.50, 'Placki ziemniaczane, śmietana', []),
         ('Fasolka po bretońsku', 9.00, '', [('500ml', 0)]),
         ('Flaki wołowe', 9.00, '', [('500ml', 0)]),
-    ],
-    'Dania rybne': [
         ('Ryba dorsz', 17.50, '2 kawałki ryby, ziemniaki lub frytki, zestaw surówek', []),
         ('Ryba miruna', 16.00, '2 kawałki ryby, ziemniaki lub frytki, zestaw surówek', []),
-    ],
-    'Zupy': [
-        ('Barszcz czerwony z uszkami', 7.50, 'barszcz, 12 uszek', [('500ml', 0)]),
-        ('Żurek z jajkiem i kiełbasą', 6.50, 'jajko, kiełbasa', [('500ml', 0)]),
-        ('Barszcz czerwony solo', 6.00, '', [('500ml', 0), ('300ml', -2.00)]),
-        ('Żurek solo', 4.50, '', [('200ml', 0)]),
-    ],
-    'Naleśniki': [
         ('Naleśniki z serem', 5.50, '', []),
-        ('Naleśniki z dżemem truskawkowym', 5.50, '', []),
-        ('Naleśniki z czekoladą', 5.50, '', []),
-        ('Naleśniki z serem i musem', 6.50, '', []),
-    ],
-    'Pierogi': [
+        ('Naleśniki z dżemem truskawkowym', 5.50, 'Naleśniki, dżem truskawkowym', []),
+        ('Naleśniki z czekoladą', 5.50, 'Naleśniki, czekolada', []),
+        ('Naleśniki z serem i musem', 6.50, 'Naleśniki z serem i musem', []),
         ('Pierogi ruskie', 9.50, '', [('12szt', 0)]),
         ('Pierogi z kapustą', 9.50, '', [('12szt', 0)]),
         ('Pierogi z mięsem', 10.00, '', [('12szt', 0)]),
@@ -37,8 +25,14 @@ MENU = {
         ('Pierogi z jabłkiem', 8.50, '', [('12szt', 0)]),
         ('Pierogi ze słodkim serem', 9.50, 'mus truskawkowy', [('12szt', 0)]),
     ],
+    'Zupy': [
+        ('Barszcz czerwony z uszkami', 7.50, 'barszcz, 12 uszek', [('500ml', 0)]),
+        ('Żurek z jajkiem i kiełbasą', 6.50, 'jajko, kiełbasa', [('500ml', 0)]),
+        ('Barszcz czerwony solo', 6.00, '', [('500ml', 0), ('300ml', -2.00)]),
+        ('Żurek solo', 4.50, '', [('200ml', 0)]),
+    ],
+    
 }
-
 
 class Command(BaseCommand):
     help = 'Seed menu Bar Mleczny Wojtek Gorlice'
